3d22344b19f9b2c73ef24acb6987af2b68ca53bb
dristanta-silwal <{dristantasilwal003@gmail.com}+{dristanta-silwal}@users.noreply.github.com>
2022-11-01 18:19:41 +0545

    color changing text added

--- src/constants/Home.jsx
+++ src/constants/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Home = () => {
-    return (
-        <div className='ssas'>
-            <h1>This is home</h1>
-        </div>
-    )
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            colors: ["navy", "orange", "blue", "green", "purple", "brown", "royalblue", "navy","burlywood", "chocolate", "crimson"],
+            sampleMessage: "This is just a collection of projects created by Dristanta Silwal.",
+            refresh: 0
+        };
+    }
+
+    refresh() {
+        let { refresh } = this.state;
+        refresh++;
+        this.setState({ refresh });
+    }
+
+    componentDidMount() {
+        setInterval(this.refresh.bind(this), 1000);
+    }
+
+    render() {
+        const { sampleMessage, colors } = this.state;
+
+        const randomColor = () => {
+            return colors[(Math.random() * colors.length) >> 0];
+        };
+        return (
+            <div className='home'>
+                {sampleMessage.split(" ").map(word => {
+                    return <span style={{ color: randomColor() }}>{`${word}`}&nbsp;</span>
+                })}
+            </div>
+        )
+    }
 }
-
 export default Home
